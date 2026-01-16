0fcc33ea00b41d41dd605acec3e99beb53551104
StanfordLing <slwx88@gmail.com>
2026-01-16 15:50:37 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,13 @@
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open('loan').then(c=>c.addAll(['./','index.html'])));
+const cacheName = 'loantracker-cache-v1';
+const filesToCache = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
 });
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
